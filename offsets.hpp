8e75a3252bde67e135fb0393b082901a1e91d434
NoobLikesThis <bigandypandy@hotmail.com>
2026-09-05 15:31:40 +0100

    Add files via upload

---
+++ offsets.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace FFlagOffsets
+{
+    uintptr_t FFlagList = 0x7FE0A48;
+    uintptr_t ValueGetSet = 0x30;
+    uintptr_t FlagToValue = 0xC0;
+}
+
+namespace FFlags
+{
+    uintptr_t JUMPfflag = 0x213BDC0;
+    uintptr_t DFIntSimAdaptiveHumanoidPDControllerSubstepMultiplier = 0x2140700;
+    uintptr_t DFIntDebugDefaultTargetWorldStepsPerFrame = 0x214067C
+    uintptr_t DFIntRunningBaseOrientationP = 0x2146E84
+    uintptr_t DFIntNewRunningBaseGravityReductionFactorHundredth = 0x23157A8
+    uintptr_t FIntPGSPenetrationMarginMax = 0x2148530
+    uintptr_t FIntPGSPenetrationMarginMin = 0x2148538
+    uintptr_t DFIntRunningBaseOrientationP = 0x2146E84
+    uintptr_t DFIntFreeFallOrientationP = 0x2146FE8
+    uintptr_t DFIntFreeFallBalanceP = 0x2146FF0
+    uintptr_t DFIntUnstickForceAttackInTenths = 0x2146FD0
+    uintptr_t DFIntNonSolidFloorPercentForceApplication = 0x2146EE0
+    uintptr_t DFIntPGSJumpForceAdjustment = 0x2147038
+    uintptr_t DFIntMaxAltitudePDStickHipHeightPercent = 0x2146EC8
+    uintptr_t DFIntSolidFloorPercentForceApplication = 0x2146ED8
+    uintptr_t FIntPGSAngularDampingPermilPersecond = 0x2148568
+    uintptr_t DFIntS2PhysicsSenderRate = 0x215712C
+    uintptr_t FIntRenderShadowIntensity = 0x2161C84
+    uintptr_t DFIntHipHeightClamp = 0x2143468
+    uintptr_t DFIntMaxAltitudePDHipHeightPercent = 0x2146EC8
+    uintptr_t DFIntJumpPowerInstantControllerMultiplierPercent =0x2147040
+    uintptr_t TaskSchedulerTargetFps = 0x7C504EC;
+
+}
